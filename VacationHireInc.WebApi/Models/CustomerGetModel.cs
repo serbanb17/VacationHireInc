35d57ac2ebe3d6e7a95a17c0c874129ca2237407
Șerban <serbanb17@gmail.com>
2021-10-14 17:45:09 +0300

    Customer API models

---
+++ VacationHireInc.WebApi/Models/CustomerGetModel.cs
@@ -0,0 +1,43 @@
+﻿// Vacation Hire Inc.
+// No copyright for this product. Intended for educational purposes.
+// Feel free to modify / share the code!
+
+using System;
+using VacationHireInc.DataLayer.Models;
+
+namespace VacationHireInc.WebApi.Models
+{
+    public class CustomerGetModel
+    {
+        public CustomerGetModel()
+        {
+        }
+
+        public CustomerGetModel(Customer customer)
+        {
+            Id = customer.Id;
+            Name = customer.Name;
+            Surname = customer.Surname;
+            Email = customer.Email;
+            PhoneNumber = customer.PhoneNumber;
+            Birthday = customer.Birthday;
+            Country = customer.Country;
+            County = customer.County;
+            City = customer.City;
+            ZipCode = customer.ZipCode;
+            Address = customer.Address;
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Country { get; set; }
+        public string County { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+        public string Address { get; set; }
+    }
+}
