b45fc84dfd40a5a5378bdf5de26bb1393f203a91
Șerban <serbanb17@gmail.com>
2021-10-29 20:07:20 +0300

    update UserController

--- VacationHireInc.WebApi/Controllers/UserController.cs
+++ VacationHireInc.WebApi/Controllers/UserController.cs
@@ -4,12 +4,13 @@
 
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using VacationHireInc.BusinessLayer.Interfaces;
+using VacationHireInc.BusinessLayer.Logic;
+using VacationHireInc.BusinessLayer.Models;
 using VacationHireInc.DataLayer.Interfaces;
 using VacationHireInc.DataLayer.Models;
 using VacationHireInc.Security.Interfaces;
-using VacationHireInc.WebApi.Models;
+using VacationHireInc.WebApi.Extensions;
 
 namespace VacationHireInc.WebApi.Controllers
 {
@@ -17,174 +18,77 @@ namespace VacationHireInc.WebApi.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        private readonly IDataAccessProvider _dataAccessProvider;
-        private readonly IJwtHelper _jwtHelper;
-        private readonly IHashingHelper _hashingHelper;
-
+        private readonly IUserLogic _userLogic;
+        
         public UserController(IDataAccessProvider dataAccessProvider, IJwtHelper jwtHelper, IHashingHelper hashingHelper)
         {
-            _dataAccessProvider = dataAccessProvider;
-            _jwtHelper = jwtHelper;
-            _hashingHelper = hashingHelper;
+            _userLogic = new UserLogic(dataAccessProvider, jwtHelper, hashingHelper);
         }
 
         [HttpGet("{id}")]
         public IActionResult Get([FromRoute] Guid id)
         {
-            User user = _dataAccessProvider.UserRepository.Get(id);
-            if (user is null)
-                return NotFound();
-            var userGetModel = new UserGetModel(user);
-            return Ok(userGetModel);
+            return _userLogic.Get(id).GetActionResult();
         }
 
         [HttpGet("byusername/{username}")]
         public IActionResult GetByUserName([FromRoute] string username)
         {
-            User user = _dataAccessProvider.UserRepository.GetByCondition(c => c.UserName == username).FirstOrDefault();
-            if (user is null)
-                return NotFound();
-            var userGetModel = new UserGetModel(user);
-            return Ok(userGetModel);
+            return _userLogic.GetByUserName(username).GetActionResult();
         }
 
         [HttpGet("byemail{email}")]
         public IActionResult GetByEmail([FromRoute] string email)
         {
-            User user = _dataAccessProvider.UserRepository.GetByCondition(c => c.Email == email).FirstOrDefault();
-            if (user is null)
-                return NotFound();
-            var userGetModel = new UserGetModel(user);
-            return Ok(userGetModel);
+            return _userLogic.GetByEmail(email).GetActionResult();
         }
 
         [HttpGet("byprivilege/{privilege}")]
         public IActionResult GetByPrivilege([FromRoute] Privilege privilege)
         {
-            List<User> users = _dataAccessProvider.UserRepository.GetByCondition(c => c.Privilege == privilege).ToList();
-            users.ForEach(u => u.Password = string.Empty);
-            var userGetModels = users.Select(u => new UserGetModel(u)).ToList();
-            return Ok(userGetModels);
+            return _userLogic.GetByPrivilege(privilege).GetActionResult();
         }
 
         [HttpGet("privileges")]
         public IActionResult GetPrivileges()
         {
-            List<string[]> privileges = Enum.GetValues(typeof(Privilege)).Cast<Privilege>().Select(p => new[] { ((int)p).ToString(), p.ToString() }).ToList();
-            return Ok(privileges);
+            return _userLogic.GetPrivileges().GetActionResult();
         }
 
         [HttpGet("count")]
         public IActionResult GetCount()
         {
-            int userCount = _dataAccessProvider.UserRepository.GetCount();
-            return Ok(userCount);
+            return _userLogic.GetCount().GetActionResult();
         }
 
         [HttpGet("page/{pageid}/{pagesize}")]
         public IActionResult GetPage([FromRoute] int pageid, [FromRoute] int pagesize)
         {
-            List<User> usersPage = _dataAccessProvider.UserRepository.GetPage(pageid, pagesize).ToList();
-            var userGetModels = usersPage.Select(u => new UserGetModel(u)).ToList();
-            return Ok(userGetModels);
+            return _userLogic.GetPage(pageid, pagesize).GetActionResult();
         }
 
         [HttpPost("authenticate")]
         public IActionResult Authenticate([FromBody] UserAuthenticateModel user)
         {
-            User dbUser = _dataAccessProvider.UserRepository.GetByCondition(u => u.UserName == user.UserName)?.FirstOrDefault();
-            if (dbUser is null)
-                return NotFound($"UserName not found!");
-
-            string hashedPassword = _hashingHelper.SaltHash(user.Password);
-            if (hashedPassword != dbUser.Password)
-                return Unauthorized($"Wrong password!");
-
-            string token = _jwtHelper.GetJwt(dbUser.Id);
-            return Ok(token);
+            return _userLogic.Authenticate(user).GetActionResult();
         }
 
         [HttpPost]
         public IActionResult Create([FromHeader(Name = "Authorization")] string token, [FromBody] UserCreateModel newUser)
         {
-            if (!_jwtHelper.IsJwtValid(token, true, out Guid userId))
-                return Unauthorized();
-
-            User user = _dataAccessProvider.UserRepository.Get(userId);
-            if (user.Privilege != Privilege.Admin)
-                return Forbid("Only allowed for admin privileged user!");
-
-            newUser.UserName = newUser.UserName.ToLower();
-            newUser.Email = newUser.Email.ToLower();
-            User duplicateUser = _dataAccessProvider.UserRepository.GetByCondition(u => u.UserName == newUser.UserName || u.Email == newUser.Email).FirstOrDefault();
-            if (duplicateUser != null)
-                return BadRequest("A user with same email and/or password exists!");
-
-            string hashedPassword = _hashingHelper.SaltHash(newUser.Password);
-            newUser.Password = hashedPassword;
-            _dataAccessProvider.UserRepository.Create(newUser.GetUser());
-            _dataAccessProvider.Save();
-
-            return Created("", null);
+            return _userLogic.Create(token, newUser).GetActionResult();
         }
 
         [HttpPut]
         public IActionResult Update([FromHeader(Name = "Authorization")] string token, [FromBody] UserUpdateModel updatedUser)
         {
-            if (!_jwtHelper.IsJwtValid(token, true, out Guid userId))
-                return Unauthorized();
-
-            User user = _dataAccessProvider.UserRepository.Get(userId);
-            if (user.Privilege != Privilege.Admin)
-                return Forbid("Only allowed for admin privileged user!");
-
-            User duplicateUser = _dataAccessProvider.UserRepository.GetByCondition(u => 
-                u.Id != updatedUser.Id 
-                && (u.UserName == updatedUser.UserName || u.Email == updatedUser.Email)
-            ).FirstOrDefault();
-            if (duplicateUser != null)
-                return BadRequest("A user with same email and/or password exists!");
-
-            updatedUser.UserName = updatedUser.UserName.ToLower();
-            updatedUser.Email = updatedUser.Email.ToLower();
-
-            User userToUpdate = _dataAccessProvider.UserRepository.Get(updatedUser.Id);
-            if (userToUpdate == null)
-                return BadRequest("Could not find user to update!");
-
-            if(userToUpdate.Privilege == Privilege.Admin && user.Id != userToUpdate.Id)
-                return Forbid("Admin privileged users can only be updated by themselves!");
-
-            string hashedPassword = _hashingHelper.SaltHash(updatedUser.Password);
-            updatedUser.Password = hashedPassword;
-
-            _dataAccessProvider.UserRepository.Update(updatedUser.SetUser(userToUpdate));
-            _dataAccessProvider.Save();
-
-            return Ok(userToUpdate.Id);
+            return _userLogic.Update(token, updatedUser).GetActionResult();
         }
 
         [HttpDelete]
         public IActionResult Delete([FromHeader(Name = "Authorization")] string token, [FromBody] Guid id)
         {
-            if (!_jwtHelper.IsJwtValid(token, true, out Guid userId))
-                return Unauthorized();
-
-            User user = _dataAccessProvider.UserRepository.Get(userId);
-            if (user.Privilege != Privilege.Admin)
-                return Forbid("Only allowed for admin privileged user!");
-
-            User userToDelete = _dataAccessProvider.UserRepository.Get(id);
-            if (userToDelete == null)
-                return BadRequest("Could not find user to delete!");
-
-            if (userToDelete.Privilege == Privilege.Admin && userToDelete.Id != userId)
-                return Forbid("Admin privileged users can only be deleted by themselves!");
-
-            _dataAccessProvider.UserRepository.Delete(new User { Id = id });
-            _dataAccessProvider.Save();
-
-            return Ok(id);
+            return _userLogic.Delete(token, id).GetActionResult();
         }
     }
 }
